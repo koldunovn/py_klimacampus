b32dd3db502a5722361af9d94b417b649d300ffa
Nikolay Koldunov <nikolay.koldunov@zmaw.de>
2014-06-23 19:48:33 +0200

    add content for meeting 8

---
+++ meeting_008/t.py
@@ -0,0 +1,25 @@
+import schedule
+import time
+import os
+from ftplib import FTP
+
+def job():
+    print("I'm working...")
+    os.system('runipy -o plot_nrt_seaice.ipynb')
+    os.system('ipython nbconvert --to html --template full_noinput.tpl plot_nrt_seaice.ipynb')
+
+    ftp = FTP('ftp.zmaw.de')
+    ftp.login(user='guest', passwd='guest')
+    ftp.cwd('outgoing/koldunov')
+    ffile = open('plot_nrt_seaice.html','rb') 
+    ftp.storbinary('STOR plot_nrt_seaice.html', ffile)
+    ffile.close()
+    ftp.quit()
+
+schedule.every(30).minutes.do(job)
+schedule.run_all()
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
